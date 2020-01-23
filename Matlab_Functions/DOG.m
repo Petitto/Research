441edc0fb70ba97069a006f5de7859774ae65079
Gerardo Petitto <petitto.gerardo@yahoo.com>
2020-01-23 13:18:04 -0600

    Changed the standard deviation resulting in more blurring and better difference of gaussian

--- Matlab_Functions/DOG.m
+++ Matlab_Functions/DOG.m
@@ -2,9 +2,18 @@
 img = imread(fullfile(img1_dname, img1));
 img = im2double(img(:,:,1));
 
-blur1 = fspecial('gaussian', 21, 15);
-blur2 = fspecial('gaussian', 21, 20);
+blur1 = fspecial('gaussian', 21, 0.5);
+blur2 = fspecial('gaussian', 21, 30);
+disp("test");
+disp(blur1);
+disp("testover");
+dogimage1 = conv2(img, blur1, 'same');
+dogimage2 = conv2(img, blur2, 'same');
+dogimage3 = dogimage1-dogimage2;
+subplot(2,2,1),imshow(I),title('original image');
 
-diffOfGaus = blur1 - blur2;
-dogimage = conv2(img, diffOfGaus, 'same');
-figure, imshow(dogimage, []);
+subplot(2,2,2),imshow(dogimage1), title('First Blur');
+
+subplot(2,2,3),imshow(dogimage2),title('Second Blur');
+
+subplot(2,2,4),imshow(dogimage3), title("Diff of Gaussian");
