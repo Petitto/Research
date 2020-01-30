a882a5d815c804091c3b1ee6b6403052633393fa
Gerardo Petitto <petitto.gerardo@yahoo.com>
2020-01-30 14:46:56 -0600

    Difference of Gaussian complete, debug keypoint search

--- Matlab_Functions/DOG.m
+++ Matlab_Functions/DOG.m
@@ -4,9 +4,9 @@ img = rgb2gray(img);
 img = im2double(img(:,:,1));
 %scale extrema and difference of gaussian
 %blur1 = fspecial('gaussian', 21, 1);
-blur = fspecial('gaussian', 21, 5);
+blur = fspecial('gaussian', 21, sqrt(2));
 disp("test");
-disp(blur1);
+disp(blur);
 disp("testover");
 image1 = conv2(img, blur, 'same');
 image2 = conv2(image1, blur, 'same');
@@ -16,52 +16,6 @@ image4 = conv2(image3, blur, 'same');
 dog = image1-image2;
 dog1 = image2-image3;
 dog2 = image3-image4;
-
-%finding keypoints
-localMax = Max.empty;
-localMin = Max.empty;
-max = false;
-min = true;
-[m,n]  = size(img);
-for i=1:m
-    for j=1:n
-        locateMat = dog(i:i+2, j:j+2);
-        locateMat1 = dog1(i:i+2, j:j+2);
-        locateMat2 = dog3(i:i+2, j:j+2);
-        potentialPoint = locateMat1(2,2);
-        for r=1:3
-            for c=1:3
-                if(r == 1 && c ==1)
-                    if(locateMat(r,c) > potentialPoint && locateMat2(r,c) > potentialPoint)
-                        max = true;
-                    elseif(locateMat(r,c) > potentialPoint && locateMat2(r,c) > potentialPoint)
-                        min = true;
-                    else
-                        max = false;
-                        min = false; 
-                        break;
-                    end
-                    
-                elseif(locateMat(r,c) > potentialPoint && locateMat1(r,c) > potentialPoint && locateMat2(r,c) > potentialPoint)
-                     max = true;
-                elseif(locateMat(r,c) < potentialPoint && locateMat1(r,c) < potentialPoint && locateMat2(r,c) < potentialPoint)
-                    min = true;
-                else
-                    min = false;
-                    max = false;
-                    break;
-                end
-            end
-        end
-        
-        if(min)
-            append(localMin,potentialPoint);
-        elseif(max)
-            append(localMax,potentialPoint);
-        end
-    end
-end
-
 
 figure(1)
 subplot(2,2,1),imshow(img),title('original image');
@@ -80,5 +34,47 @@ subplot(2,2,2),imshow(image2), title('Blur 2');
 subplot(2,2,3),imshow(image3),title('Blur 3');
 
 subplot(2,2,4),imshow(image4), title('Blur 4');
+%finding keypoints
+localMax = zeros(1,2);
+localMin = zeros(1,2);
+max = true;
+min = true;
+[m,n]  = size(img);
+for i=1:m-2
+    for j=1:n-2
+        locateMat = dog(i:i+2, j:j+2);
+        locateMat1 = dog1(i:i+2, j:j+2);
+        locateMat2 = dog2(i:i+2, j:j+2);
+        potentialPoint = locateMat1(2,2);
+        for r=1:3
+            for c=1:3
+                if(r == 2 && c ==2)
+                    if(locateMat(r,c) > potentialPoint || locateMat2(r,c) > potentialPoint)
+                        max = false;
+                    elseif(locateMat(r,c) < potentialPoint || locateMat2(r,c) < potentialPoint)
+                        min = false;
+                    end
+                    
+                elseif(locateMat(r,c) > potentialPoint || locateMat1(r,c) > potentialPoint || locateMat2(r,c) > potentialPoint)
+                     max = false;
+                elseif(locateMat(r,c) < potentialPoint || locateMat1(r,c) < potentialPoint || locateMat2(r,c) < potentialPoint)
+                    min = false;
+                end
+            end
+        end
+        
+        if(min)
+            disp([i+1,j+1]);
+            localMin(end +1,1) = i+1;
+             localMin(end,2) = j+1;
+%             append(localMin, [i+1,j+1]);
+%         elseif(max)
+%             append(localMax, [i+1,j+1]);
+        end
+    end
+end
+
+
+
 
 
